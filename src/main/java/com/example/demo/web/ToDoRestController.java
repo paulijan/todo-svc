0806fb9e395142a5bdb6ee509148e08577ad4dce
Paulina Janoszczyk <paulina.janoszczyk@student.htw-berlin.de>
2024-06-26 10:19:44 +0200

    Enable CORS for API endpoints using @CrossOrigin annotation

--- src/main/java/com/example/demo/web/ToDoRestController.java
+++ src/main/java/com/example/demo/web/ToDoRestController.java
@@ -20,27 +20,34 @@ public class ToDoRestController {
     }
 
 
+    @CrossOrigin
     @GetMapping(path = "/api/v1/todos")  //Lesende Anfrage
     public ResponseEntity<List<ToDo>> fetchToDos(){         // Empfängt den Request
         return ResponseEntity.ok(toDoService.findAll());  //Mapping findet nicht mehr statt, es wird zum Service deligiert
     }
+    @CrossOrigin
    @GetMapping(path = "/api/v1/todos/{id}")
    public ResponseEntity<ToDo> fetchToDobyId(@PathVariable Long id){
         var toDo = toDoService.findById(id);
         return toDo != null? ResponseEntity.ok(toDo) : ResponseEntity.notFound().build(); // Bei keinem Eintrag in der DB notFound
    }
+
+    @CrossOrigin
     @PostMapping(path = "/api/v1/todos")  //Schreibende Anfrage
     public ResponseEntity<Void> createToDo(@RequestBody ToDoManipulationRequest request) throws URISyntaxException {  //Neue "Ressource" wird erstellt und unter welcher Uri ist sie erreichbar
         var toDo =  toDoService.create(request);             //Wird über Header zurück gesendet und nicht über Responsebody deshalb ist er leer (Void)
         URI uri = new URI("/api/v1/todos/" + toDo.getId());  //"Adresse" der neu erzeugten Ressource
         return ResponseEntity.created(uri).build();
     }
+
+    @CrossOrigin
     @PutMapping(path = "/api/v1/todos/{id}") //Update
     public ResponseEntity<ToDo> updateToDo(@PathVariable Long id, @RequestBody ToDoManipulationRequest request) { //Aktualisiert zurück
         var toDo = toDoService.update(id, request);
         return toDo != null? ResponseEntity.ok(toDo) : ResponseEntity.notFound().build(); // Bei keinem Eintrag in der DB notFound
     }
 
+    @CrossOrigin
     @DeleteMapping(path = "/api/v1/todos/{id}")
     public ResponseEntity<Void> deleteToDo(@PathVariable Long id){  //Void da gelöschter "Eintrag" es soll/wird nichts angezeigt
         boolean successful = toDoService.deleteById(id);
